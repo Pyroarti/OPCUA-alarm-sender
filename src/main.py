cad2db3e921ac450304e2d7afd5f0209b4b98627
Robert <robert.balulis@hotmail.com>
2023-08-21 22:06:22 +0200

    Added a way to update users info

--- src/main.py
+++ src/main.py
@@ -47,6 +47,196 @@ def run_asyncio_loop(queue):
         loop.close()
 
 
+class CreateUserWindow(customtkinter.CTkToplevel):
+    """ Class for the create a new user."""
+    def __init__(self, parent, app_instance, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.app = app_instance
+
+        window_width = 300
+        window_height = 400
+
+        parent_x = parent.winfo_x()
+        parent_y = parent.winfo_y()
+
+        parent_width = parent.winfo_width()
+        parent_height = parent.winfo_height()
+
+        pos_x = parent_x + (parent_width // 2) - (window_width // 2)
+        pos_y = parent_y + (parent_height // 2) - (window_height // 2)
+
+        self.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
+        self.resizable(False, False)
+        self.title("OPC UA Alarm Notifier")
+        self.attributes('-topmost', True)
+
+        header_label = customtkinter.CTkLabel(master=self,
+                                            text="User creator",
+                                            font=("Helvetica", 17),
+                                            bg_color="transparent")
+        header_label.pack(pady=10)
+
+        self.name_label = customtkinter.CTkLabel(self, text="Name:", bg_color="transparent", font=("Helvetica", 16))
+        self.name_label.pack()
+
+        self.name_entry = customtkinter.CTkEntry(self,
+                                                    font=("Helvetica", 16),
+                                                    width=200)
+        self.name_entry.pack()
+
+        self.phone_number_label = customtkinter.CTkLabel(self, text="Number:", bg_color="transparent", font=("Helvetica", 16))
+        self.phone_number_label.pack()
+
+        self.phone_number_entry = customtkinter.CTkEntry(self,
+                                                    font=("Helvetica", 16),
+                                                    width=200)
+        self.phone_number_entry.pack()
+
+        self.submit_button = customtkinter.CTkButton(self,
+                                                     text="Submit the user",
+                                                     command=self.check_user_input,
+                                                     width=200,
+                                                     height=40,
+                                                     font=("Helvetica", 18))
+        self.submit_button.pack(pady=10)
+
+
+    def check_user_input(self):
+        """Checks the user input"""
+        name = self.name_entry.get()
+        phone_number = self.phone_number_entry.get()
+
+        if name == "" or phone_number == "":
+            messagebox.showerror("Error", "Please fill out all the fields")
+            return
+
+        with open(phone_book_file, "r",encoding="UTF8") as file:
+            data = file.read()
+            json_data = json.loads(data)
+
+            for number in json_data:
+                if number["phone_number"] == number:
+                    messagebox.showerror("Error", "This number is already in use")
+                    return
+
+        user = {
+            "Name": name,
+            "phone_number": phone_number,
+            "Active": "Yes"
+            }
+
+        with open(phone_book_file, "r",encoding="UTF8") as file:
+            data = file.read()
+            json_data = json.loads(data)
+            json_data.append(user)
+
+        with open(phone_book_file, "w",encoding="UTF8") as file:
+            json.dump(json_data, file, indent=4)
+
+        self.destroy()
+
+        self.app.update_user_list()
+
+
+class EditUserWindow(customtkinter.CTkToplevel):
+    """Class for the how to use window with the video tutorial."""
+    def __init__(self, parent, app_instance, user_name, user_number, user_active, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.user_name = user_name
+        self.user_number = user_number
+        self.user_active = user_active
+        self.app = app_instance
+
+        window_width = 300
+        window_height = 400
+
+        parent_x = parent.winfo_x()
+        parent_y = parent.winfo_y()
+
+        parent_width = parent.winfo_width()
+        parent_height = parent.winfo_height()
+
+        pos_x = parent_x + (parent_width // 2) - (window_width // 2)
+        pos_y = parent_y + (parent_height // 2) - (window_height // 2)
+
+        self.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
+        self.resizable(False, False)
+        self.title("OPC UA Alarm Notifier")
+        self.attributes('-topmost', True)
+
+        information_label = customtkinter.CTkLabel(master=self,
+                                            text="User editor",
+                                            font=("Helvetica", 17),
+                                            bg_color="transparent")
+        information_label.pack(pady=10)
+
+        self.new_name_label = customtkinter.CTkLabel(self, text="Name:", bg_color="transparent", font=("Helvetica", 16))
+        self.new_name_label.pack()
+
+        self.new_name_entry = customtkinter.CTkEntry(self,
+                                                    font=("Helvetica", 16),
+                                                    width=200)
+        self.new_name_entry.pack()
+        self.new_name_entry.insert(0, self.user_name)
+
+        self.new_phone_number_label = customtkinter.CTkLabel(self, text="Number:", bg_color="transparent", font=("Helvetica", 16))
+        self.new_phone_number_label.pack()
+
+        self.new_phone_number_entry = customtkinter.CTkEntry(self,
+                                                    font=("Helvetica", 16),
+                                                    width=200,)
+        self.new_phone_number_entry.pack()
+        self.new_phone_number_entry.insert(0, self.user_number)
+
+        self.submit_button = customtkinter.CTkButton(self,
+                                                     text="Submit the user",
+                                                     command=self.check_user_input,
+                                                     width=200,
+                                                     height=40,
+                                                     font=("Helvetica", 18))
+        self.submit_button.pack(pady=10)
+
+
+    def check_user_input(self):
+        """Checks the user input"""
+        new_name = self.new_name_entry.get()
+        new_phone_number = self.new_phone_number_entry.get()
+
+        if new_name == "" or new_phone_number == "":
+            messagebox.showerror("Error", "Please fill out all the fields")
+            return
+
+        user = {
+            "Name": new_name,
+            "phone_number": new_phone_number,
+            "Active": "Yes"
+        }
+
+        with open(phone_book_file, "r", encoding="UTF8") as file:
+            data = file.read()
+            json_data = json.loads(data)
+
+        # Find the index of the user to edit
+        user_index = None
+        for i, current_user in enumerate(json_data):
+            if current_user["Name"] == self.user_name and current_user["phone_number"] == self.user_number:
+                user_index = i
+                break
+
+        # If user found, update its data
+        if user_index is not None:
+            json_data[user_index] = user
+            with open(phone_book_file, "w", encoding="UTF8") as file:
+                json.dump(json_data, file, indent=4)
+        else:
+            messagebox.showerror("Error", "User not found")
+
+        self.destroy()
+        self.app.update_user_list()
+
+
 class AboutWindow(customtkinter.CTkToplevel):
     """Class for the how to use window with the video tutorial."""
     def __init__(self, parent, *args, **kwargs):
@@ -77,40 +267,6 @@ class AboutWindow(customtkinter.CTkToplevel):
                                             bg_color="transparent")
         information_label.pack(pady=10)
 
-class EditUserWindow(customtkinter.CTkToplevel):
-    """Class for the how to use window with the video tutorial."""
-    def __init__(self, parent, user_name, user_number, user_active, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.user_name = user_name
-        self.user_number = user_number
-        self.user_active = user_active
-
-        window_width = 300
-        window_height = 400
-
-        parent_x = parent.winfo_x()
-        parent_y = parent.winfo_y()
-
-        parent_width = parent.winfo_width()
-        parent_height = parent.winfo_height()
-
-        pos_x = parent_x + (parent_width // 2) - (window_width // 2)
-        pos_y = parent_y + (parent_height // 2) - (window_height // 2)
-
-        self.geometry(f"{window_width}x{window_height}+{pos_x}+{pos_y}")
-        self.resizable(False, False)
-        self.title("OPC UA Alarm Notifier")
-        self.attributes('-topmost', True)
-
-        information_label = customtkinter.CTkLabel(master=self,
-                                            text="User editor",
-                                            font=("Helvetica", 17),
-                                            bg_color="transparent")
-        information_label.pack(pady=10)
-
-
-
 
 class App(customtkinter.CTk):
     """Class for the main app and main window"""
@@ -130,6 +286,7 @@ class App(customtkinter.CTk):
 
         self.toplevel_window = None
         self.edit_user_window = None
+        self.create_new_user_window = None
 
         self.main_frame = customtkinter.CTkFrame(master=self, bg_color="black")
         self.main_frame.pack(pady=20, padx=40, fill="both")
@@ -141,7 +298,7 @@ class App(customtkinter.CTk):
         self.main_label.pack(pady=10, padx=10)
 
         self.create_new_user_button = customtkinter.CTkButton(master=self.main_frame,
-                                                 command="",
+                                                 command=self.open_create_user_window,
                                                  text="Create new user",
                                                  width=200,
                                                  height=50,
@@ -208,6 +365,29 @@ class App(customtkinter.CTk):
 
         self.users_treeview.bind("<Double-1>", self.open_edit_user_window)
 
+    def update_user_list(self):
+        """Updates the user list"""
+
+        for user in self.users_treeview.get_children():
+            self.users_treeview.delete(user)
+
+        with open(phone_book_file, "r",encoding="UTF8") as file:
+            data = file.read()
+            json_data = json.loads(data)
+
+            for user in json_data:
+                Name = user["Name"]
+                phone_number = user["phone_number"]
+                user_active = user["Active"]
+                self.users_treeview.insert("", "end", text="", values=(Name, phone_number, user_active))
+
+
+    def open_create_user_window(self):
+        """Opens the how to use page"""
+        if self.create_new_user_window is None or not self.create_new_user_window.winfo_exists():
+            self.create_new_user_window = CreateUserWindow(self, self)
+        self.create_new_user_window.lift()
+
 
     def open_about_window(self):
         """Opens the how to use page"""
@@ -225,7 +405,7 @@ class App(customtkinter.CTk):
         user_active = self.users_treeview.item(item, "values")[2]
 
         if self.edit_user_window is None or not self.edit_user_window.winfo_exists():
-            self.edit_user_window = EditUserWindow(self, user_name, user_number, user_active)
+            self.edit_user_window = EditUserWindow(self, self, user_name, user_number, user_active)
         self.edit_user_window.lift()
 
 
